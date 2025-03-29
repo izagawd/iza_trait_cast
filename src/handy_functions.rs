95065143ba25c9507bd62df702882659dc11adee
Emmanuel Onyekwelu <30781523+izagawd@users.noreply.github.com>
2025-03-29 20:47:42 -0300

    made optimizations

--- src/handy_functions.rs
+++ src/handy_functions.rs
@@ -1,9 +1,23 @@
+use std::any::{type_name, Any};
+
 pub(crate) unsafe fn generic_transmute<T, U>(t: T) -> U {
-    const { assert!(size_of::<T>() == size_of::<U>(),"To transmute, both types must be of the same size") }; // sanity check
+    const { assert!(size_of::<T>() <= size_of::<U>(),"To transmute, both types must be of the same size") }; // sanity check
     let t = core::mem::ManuallyDrop::new(t);
     core::mem::transmute_copy(&t)
 }
+pub  const  fn is_trait_generic<T: ?Sized>() -> bool {
 
+    let splitted_at = type_name::<T>().split_at_checked(4);
+    match splitted_at {
+        None => {
+            false
+        }
+        Some(splitted_at) => {
+            return  comptime_str_eq(splitted_at.0, "dyn ") && size_of::<&T>() == size_of::<&dyn Any>();
+        }
+    }
+
+}
 
 pub const fn comptime_str_eq(a: &str, b: &str) -> bool {
 
