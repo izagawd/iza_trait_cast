374b2ca798806c01045147352024dedb2fd5c257
Emmanuel Onyekwelu <30781523+izagawd@users.noreply.github.com>
2025-03-29 21:33:57 -0300

    made optimizations

--- src/cast_fns.rs
+++ src/cast_fns.rs
@@ -5,13 +5,13 @@ use crate::handy_functions::comptime_str_eq;
 use std::any::Any;
 // macro, to avoid repeating code
 macro_rules! cast_reference {
-    ($TTo:ty, $from:expr, $from_ty:ty, $reg:expr) => {
+    ($TTo:ty, $from:expr,  $reg:expr) => {
         {
 
             // checks if generic input is a dyn T
             const {
                 assert!(is_trait_generic::<$TTo>(),"TTo must be a dyn Trait!! eg:\n cast::<dyn Any>(&from)");
-                assert!(is_trait_generic::<$from_ty>(),"Inputed type to cast must be a dyn Trait!! eg:\n cast::<dyn Any>(&from)")
+
             }
 
             let vtable =  get_vtable::<$TTo>($from,$reg);
@@ -28,7 +28,7 @@ macro_rules! cast_reference {
                     to_v.1 = vtable;
                     Ok(unsafe{generic_transmute(
                         to_v
-                    )})
+                    )}) 
                 }
                 Err(err) => {
                     Err(err)
@@ -38,19 +38,14 @@ macro_rules! cast_reference {
     };
 }
 
-fn cast_ref_hidden<'a,TTo: ?Sized + 'static, TFrom: Castable + ?Sized>(from: &TFrom, reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a TTo, VTableError> {
-    cast_reference!(TTo,from,TFrom,reg)
+
+
+pub fn cast_ref<'a,TTo: ?Sized + 'static>(from: &dyn Castable, reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a TTo, VTableError> {
+    cast_reference!(TTo,from,reg)
 }
 
 
-pub fn cast_ref<'a,TTo: ?Sized + 'static>(from: &(impl Castable + ?Sized), reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a TTo, VTableError> {
-    cast_ref_hidden(from, reg)
-}
-
-fn cast_mut_hidden<'a,TTo: ?Sized + 'static, TFrom: Castable + ?Sized>(from: &mut TFrom, reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a mut TTo, VTableError> {
-    cast_reference!(TTo,from,TFrom,reg)
-}
-pub fn cast_mut<'a,TTo: ?Sized + 'static>(from: &mut (impl Castable + ?Sized), reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a mut TTo, VTableError> {
-    cast_mut_hidden(from, reg)
+pub fn cast_mut<'a,TTo: ?Sized + 'static>(from: &mut dyn  Castable, reg: &TraitVTableRegistry<impl TraitVTableRegisterer>) -> Result<&'a mut TTo, VTableError> {
+    cast_reference!(TTo,from,reg)
 }
 
